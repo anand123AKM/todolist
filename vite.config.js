49fda2de35b7a5e09bee2c361db971414ca94d61
anand maurya <anandkr.maurya10@gmail.com>
2025-04-03 11:38:58 +0530

    coonverted in the PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["/bgs.jpg"],
+      manifest: {
+        name: "Task Master",
+        short_name: "Task Master",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "//bgs.jpg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "//bgs.jpg",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
